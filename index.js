5ce9428476f37158ed265f2a9ccc3ba6c49ce86d
pc <vanshikakhandelwal102@gmail.com>
2026-07-23 15:43:17 +0530

    Day 2: Added Home page, Add Blog page, navbar and footer

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
+// const express = require("express");
+
+// const app = express();
+
+// app.get("/", (req, res) => {
+//   res.send("Hello World!");
+// });
+
+// app.listen(3000, () => {
+//   console.log("Server is running on http://localhost:3000");
+// });
+
+
 const express = require("express");
+
+const path = require("path");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.use(express.static(path.join(__dirname, "public")));
 
 app.listen(3000, () => {
-  console.log("Server is running on http://localhost:3000");
+    console.log("Server Running at http://localhost:3000");
 });
